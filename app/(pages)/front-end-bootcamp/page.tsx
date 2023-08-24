eba1c1361765c57c13e26b49a8bdbc4db64bf6b1
cristian-albu <albucristian95p@gmail.com>
2023-08-24 08:56:59 +0300

    fronted bootcamp partial structure

---
+++ app/(pages)/front-end-bootcamp/page.tsx
@@ -0,0 +1,272 @@
+import React from "react";
+
+const FrontEndBootCamp = () => {
+    return (
+        <div className="p-10 flex flex-col">
+            <h1 className="text-5xl mb-5">Front end bootcamp</h1>
+
+            <h2 className="text-2xl">Workspace setup</h2>
+            <ol className="list-decimal pl-5">
+                <li></li>
+            </ol>
+
+            <h2 className="text-2xl">Basic JavaScript</h2>
+            <ol className="list-decimal pl-5">
+                <li></li>
+                <li>Callbacks</li>
+            </ol>
+
+            <h2 className="text-2xl">ES6-ES6</h2>
+            <ol className="list-decimal pl-5">
+                <li>let and const</li>
+                <li>Arrow Functions</li>
+                <li>Array.forEach</li>
+                <li>Array.map</li>
+                <li>Array.filter</li>
+                <li>Array.slice</li>
+                <li>Array.splice</li>
+                <li>Array.reduce</li>
+                <li>Array.sort</li>
+                <li>Array.every</li>
+                <li>Array.some</li>
+                <li>Array.indexOf</li>
+                <li>Array.isArray</li>
+                <li>Array.find</li>
+                <li>for/of</li>
+                <li>The spread operator</li>
+                <li>Object entries</li>
+                <li>Maps</li>
+                <li>Sets</li>
+                <li>Object entries</li>
+            </ol>
+
+            <h2 className="text-2xl">RegEx</h2>
+            <ol className="list-decimal pl-5">
+                <li>search</li>
+                <li>replace</li>
+                <li>common patterns</li>
+                <li>test</li>
+            </ol>
+
+            <h2 className="text-2xl">Algorithms & Data Structures I</h2>
+            <ol className="list-decimal pl-5">
+                <li>Symmetric Difference</li>
+                <li>Inventory Update</li>
+                <li>No repeats please</li>
+                <li>Pairwise</li>
+                <li>Contains Duplicate</li>
+                <li>Valid Anagram</li>
+                <li>Two Sum</li>
+                <li>Group Anagrams</li>
+                <li>Top K Frequent Elements</li>
+                <li>Product of Array Except Self</li>
+                <li>Valid Sudoku</li>
+                <li>Valid Palindrome</li>
+                <li>Longest Consecutive Sequence</li>
+                <li>Two Sum II Input Array is Sorted</li>
+                <li>Typed Arrays</li>
+                <li>Learn how a Stack Works</li>
+                <li>Create a Stack Class</li>
+                <li>Create a Queue Class</li>
+                <li>Create a Priority Queue</li>
+                <li>Create a Circular Queue</li>
+                <li>Create a Set Class</li>
+                <li>Create a Union on Two Sets</li>
+                <li>Perform an Intersection on Two Sets of Data</li>
+                <li>Perform an Difference on Two Sets of Data</li>
+                <li>Perform an Subset Check on Two Sets of Data</li>
+                <li>Create a Map Data Structure</li>
+                <li>Create a Hash Table</li>
+            </ol>
+
+            <h2 className="text-2xl">HTML & CSS: Beginner</h2>
+            <ol className="list-decimal pl-5">
+                <li>Head and Body</li>
+                <li>Headings & paragraphs</li>
+                <li>Lists</li>
+                <li>Images</li>
+                <li>Anchors and buttons</li>
+                <li>Divs and spans</li>
+                <li>Script</li>
+            </ol>
+
+            <h2 className="text-2xl">The DOM</h2>
+            <ol className="list-decimal pl-5">
+                <li></li>
+            </ol>
+
+            <h2 className="text-2xl">First project: Basic Website</h2>
+            <ol className="list-decimal pl-5">
+                <li></li>
+            </ol>
+
+            <h2 className="text-2xl">Modules & Packages</h2>
+            <ol className="list-decimal pl-5">
+                <li></li>
+            </ol>
+
+            <h2 className="text-2xl">Basic TypeScript</h2>
+            <ol className="list-decimal pl-5">
+                <li></li>
+            </ol>
+
+            <h2 className="text-2xl">Testing with Jest</h2>
+            <ol className="list-decimal pl-5">
+                <li></li>
+            </ol>
+
+            <h2 className="text-2xl">Object Oriented Programming</h2>
+            <ol className="list-decimal pl-5">
+                <li></li>
+            </ol>
+
+            <h2 className="text-2xl">Algorithms & Data Structures II</h2>
+            <ol className="list-decimal pl-5">
+                <li>Bubble sort</li>
+                <li>Selection sort</li>
+                <li>Insertion sort</li>
+                <li>Quick sort</li>
+                <li>Merge sort</li>
+                <li>Binary search</li>
+                <li>Work with nodes in a Linked List</li>
+                <li>Create a Linked List Class</li>
+                <li>Search within a Linked List</li>
+                <li>Remove Elements from a Linked List by Index</li>
+                <li>Add Elements at a specific index in a Linked List</li>
+                <li>Create a Doubly Linked List</li>
+                <li>Reverse a Doubly Linked List</li>
+                <li>3Sum</li>
+                <li>Container With Most Water</li>
+                <li>Trapping Rain Water</li>
+                <li>Valid Parentheses</li>
+                <li>Min Stack</li>
+                <li>Evaluate Reverse Polish Notation</li>
+                <li>Generate Parentheses</li>
+                <li>Daily Temperatures</li>
+                <li>Car Fleet</li>
+                <li>Largest Rectangle in Histogram</li>
+                <li>Search a 2d Matrix</li>
+                <li>Koko Eating babanas</li>
+                <li>Find minimum in rotated sorted array</li>
+                <li>search in a rotated sorted array</li>
+                <li>Time based key value store</li>
+                <li>Median of Two sorted arrays</li>
+            </ol>
+
+            <h2 className="text-2xl">HTML & CSS: Intermediate</h2>
+            <ol className="list-decimal pl-5">
+                <li>Forms</li>
+            </ol>
+
+            <h2 className="text-2xl">Understanding APIs, databases, and servers</h2>
+            <ol className="list-decimal pl-5">
+                <li>Basic backend structure</li>
+                <li>NodeJs</li>
+                <li>SQL and NoSQL</li>
+                <li>REST apis</li>
+                <li>Basic Node app</li>
+            </ol>
+
+            <h2 className="text-2xl">Requests and Responses</h2>
+            <ol className="list-decimal pl-5">
+                <li>XMLHttp</li>
+                <li>Request</li>
+                <li>Response</li>
+                <li>JSON</li>
+                <li>Fetch API</li>
+            </ol>
+
+            <h2 className="text-2xl">Async JS</h2>
+            <ol className="list-decimal pl-5">
+                <li>How to browsers works</li>
+                <li>Callbacks reminder</li>
+                <li>Asynchronous</li>
+                <li>Promises</li>
+                <li>Async/Await</li>
+            </ol>
+
+            <h2 className="text-2xl">Second project: API website</h2>
+            <ol className="list-decimal pl-5">
+                <li></li>
+            </ol>
+
+            <h2 className="text-2xl">Algorithms & Data Structures III</h2>
+            <ol className="list-decimal pl-5">
+                <li>Best time to buy and sell stock</li>
+                <li>Longest substring without repeating characters</li>
+                <li>Longest repeating character replacement</li>
+                <li>Permutation in string</li>
+                <li>Minimum window substring</li>
+                <li>Sliding window maximum</li>
+                <li>Add a New element to a Binary Search Tree</li>
+                <li>Find the minimum and maximum value in a binary search tree</li>
+                <li>Check if an element is present in a binary search tree</li>
+                <li>Check if tree is binary search tree</li>
+                <li>Find the minimum and amximum height in a binary search tree</li>
+                <li>Use Depth First Search in a binary search tree</li>
+                <li>Use Breath first Search in a Binary Search Tree</li>
+                <li>Delete a Leaf Node in a Binary Search Tree</li>
+                <li>Delete a Node with one children in Binary Search Tree</li>
+                <li>Delete a Node with Two Children in Binary Search Tree</li>
+                <li>Invert a Binary Tree</li>
+                <li>Subsets</li>
+                <li>Combination Sum</li>
+                <li>Permutations</li>
+                <li>Subsets II</li>
+                <li>Combination Sum II</li>
+                <li>Word Search</li>
+                <li>Create a Trie Search Tree</li>
+                <li>Implement Trie Prefix Tree</li>
+                <li>Design add and search words data structure</li>
+                <li>Word Search II</li>
+            </ol>
+
+            <h2 className="text-2xl">HTML & CSS: Advanced</h2>
+            <ol className="list-decimal pl-5">
+                <li>A11Y</li>
+            </ol>
+
+            <h2 className="text-2xl">Third project: Chess website</h2>
+            <ol className="list-decimal pl-5">
+                <li></li>
+            </ol>
+
+            <h2 className="text-2xl">React basics and setup</h2>
+            <ol className="list-decimal pl-5">
+                <li>What is React</li>
+                <li>How does React work</li>
+                <li>Vite</li>
+                <li>JSX</li>
+                <li>Components</li>
+                <li>Props</li>
+                <li>Events</li>
+                <li>Conditionals</li>
+                <li>Rendering lists</li>
+                <li>Routing</li>
+            </ol>
+
+            <h2 className="text-2xl">React Class components</h2>
+            <ol className="list-decimal pl-5">
+                <li>Constructor</li>
+                <li>Mounting</li>
+                <li>Rendering</li>
+                <li>Updating</li>
+                <li>Unmounting</li>
+                <li>Class component state</li>
+            </ol>
+
+            <h2 className="text-2xl">React Hooks</h2>
+            <ol className="list-decimal pl-5">
+                <li>useState</li>
+                <li>useEffect</li>
+                <li>useRef</li>
+                <li>useCallback</li>
+                <li>useMemo</li>
+                <li>useContext</li>
+                <li>useReducer</li>
+            </ol>
+        </div>
+    );
+};
+
+export default FrontEndBootCamp;
