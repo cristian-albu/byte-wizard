7880573fa961d397ee1d0df5ac9ad926474dc224
Cristian <cristian@Cristians-Laptop.local>
2023-08-24 16:50:22 +0300

    curriculum finish

---
+++ app/(pages)/front-end-bootcamp/basic-javascript/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const BasicJavaScript = () => {
+    return <div>Part1</div>;
+};
+
+export default BasicJavaScript;
---
+++ app/(pages)/front-end-bootcamp/basic-javascript/segments/variablesAndDataTypes.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const variablesAndDataTypes = () => {
+    return <div>variablesAndDataTypes</div>;
+};
+
+export default variablesAndDataTypes;
